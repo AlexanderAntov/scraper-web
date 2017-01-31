603c2a2d08dd61198f869220b2a0968c7a662604
AlexanderAntov <a.antov91@gmail.com>
2017-01-31 21:01:19 +0200

    Added the app hosting file

--- app.js
+++ app.js
@@ -1,31 +1,4 @@
-﻿(function () {
-    'use strict';
-
-    angular.module('app', [
-        'ui.router',
-        'app.newsList',
-        'app.weatherLineChart'
-    ])
-    .config(function ($stateProvider) {
-        $stateProvider
-            .state('app', {
-                url: '/home',
-                controller: 'appCtrl',
-                templateUrl: 'index.html'
-            })
-            .state('newsList', {
-                url: '/news-list',
-                controller: 'newsListCtrl',
-                templateUrl: 'news-list/news-list.html'
-            })
-            .state('chartsWeatherLine', {
-                url: '/weather-line-chart',
-                controller: 'weatherLineChartCtrl',
-                templateUrl: 'weather-line-chart/weather-line-chart.html'
-            });
-    })
-    .controller('appCtrl', AppCtrl);
-
-    function AppCtrl() {
-    }
-})();
+﻿var express = require('express'),
+    app = express();
+app.use(express.static(__dirname));
+app.listen(process.env.PORT || 3000);
---
+++ index.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app', [
+        'ui.router',
+        'app.newsList',
+        'app.weatherLineChart'
+    ])
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('app', {
+                url: '/home',
+                controller: 'appCtrl',
+                templateUrl: 'index.html'
+            })
+            .state('newsList', {
+                url: '/news-list',
+                controller: 'newsListCtrl',
+                templateUrl: 'news-list/news-list.html'
+            })
+            .state('chartsWeatherLine', {
+                url: '/weather-line-chart',
+                controller: 'weatherLineChartCtrl',
+                templateUrl: 'weather-line-chart/weather-line-chart.html'
+            });
+    })
+    .controller('appCtrl', AppCtrl);
+
+    function AppCtrl() {
+    }
+})();

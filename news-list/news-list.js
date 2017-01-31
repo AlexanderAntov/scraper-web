0100dfce7dfb42d76e9a62bb2e854577565c0dd6
AlexanderAntov <a.antov91@gmail.com>
2017-01-31 21:08:52 +0200

    Serve the news over https

--- news-list/news-list.js
+++ news-list/news-list.js
@@ -5,7 +5,7 @@
         .controller('newsListCtrl', NewsListCtrl);
 
     function NewsListCtrl($http, $scope) {
-        $http.get('http://scraper-api.herokuapp.com/news?images="true"').then(function (response) {
+        $http.get('https://scraper-api.herokuapp.com/news?images="true"').then(function (response) {
             $scope.newsList = response.data;
         });
     }

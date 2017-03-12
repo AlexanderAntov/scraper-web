273b2281b1d4c960ee164bee1b16444e4d2e2a4f
AlexanderAntov <a.antov91@gmail.com>
2017-03-12 20:04:07 +0200

    Implemented a search functionality

--- news-list/news-list.js
+++ news-list/news-list.js
@@ -5,10 +5,39 @@
         .controller('newsListCtrl', NewsListCtrl);
 
     function NewsListCtrl($http, $scope) {
+        $scope.onSearchValueChange = onSearchValueChange;
+        $scope.$watch('searchInputVisible', cleanSearchValue);
+
         $http.get('https://scraper-api.herokuapp.com/news?images="true"').then(function (response) {
             if (response && response.data) {
                 $scope.newsList = response.data;
+                $scope.pristineNewsList = response.data;
             }
         });
+
+        function cleanSearchValue(value) {
+            if (!value) {
+                $scope.searchValue = '';
+            }
+        }
+
+        function onSearchValueChange(value) {
+            var filteredNewsList = [],
+                lowerCaseValue;
+
+            if (value && value.length > 2) {
+                lowerCaseValue = value.toLowerCase();
+                $scope.pristineNewsList.forEach(function (newsModel) {
+                    if ((newsModel.title && newsModel.title.toLowerCase().indexOf(lowerCaseValue) > -1) ||
+                        (newsModel.info && newsModel.info.toLowerCase().indexOf(lowerCaseValue) > -1)) {
+                        filteredNewsList.push(newsModel);
+                    }
+                });
+            } else {
+                filteredNewsList = $scope.pristineNewsList;
+            }
+
+            $scope.newsList = filteredNewsList;
+        }
     }
 })();

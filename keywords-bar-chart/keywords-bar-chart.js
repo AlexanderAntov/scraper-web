1bb31ecbcad387394659878bc0b8d078125d491f
AlexanderAntov <a.antov91@gmail.com>
2017-11-12 17:32:22 +0200

    Added barchart logic

--- keywords-bar-chart/keywords-bar-chart.js
+++ keywords-bar-chart/keywords-bar-chart.js
@@ -6,6 +6,56 @@
 
     function KeywordsBarChartCtrl($http) {
         $http.get('https://scraper-api.herokuapp.com/news-keywords').then(function (response) {
+            var keywordData = [];
+            response.data.slice(0, 50).forEach(function (model) {
+                keywordData.push([model.word, model.score]);
+            });
+
+            Highcharts.chart('keywords-chart-container', {
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: 'News top keywords'
+                },
+                subtitle: {
+                    text: 'top 50 keywords'
+                },
+                xAxis: {
+                    type: 'category',
+                    labels: {
+                        rotation: -45,
+                        style: {
+                            fontSize: '13px',
+                            fontFamily: 'Verdana, sans-serif'
+                        }
+                    }
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'score'
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                series: [{
+                    name: 'News score',
+                    data: keywordData,
+                    dataLabels: {
+                        enabled: true,
+                        rotation: -90,
+                        color: '#FFFFFF',
+                        align: 'right',
+                        y: 10,
+                        style: {
+                            fontSize: '13px',
+                            fontFamily: 'Verdana, sans-serif'
+                        }
+                    }
+                }]
+            });
         });
     }
 })();

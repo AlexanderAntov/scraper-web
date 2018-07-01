891030c00faf958093eb6437187190702ac4f785
AlexanderAntov <a.antov91@gmail.com>
2018-07-01 20:51:49 +0300

    Restyle the home page

--- src/components/tile-grid/tile-grid.js
+++ src/components/tile-grid/tile-grid.js
@@ -1,41 +1,37 @@
-(function () {
-    'use strict';
+angular.module('app.components.tileGrid', []).component('tileGrid', {
+    templateUrl: 'src/components/tile-grid/tile-grid.html',
+    bindings: {
+        itemsList: '<?',
+        summarize: '<?'
+    },
+    controller: class TileGridCtrl {
+        $onChanges(changes) {
+            if (changes && changes.itemsList) {
+                const itemWidth = this._getItemWidth() + 'px',
+                    tilesList = document.getElementsByClassName('item-container'),
+                    tilesListLength = tilesList.length;
+                let iter;
 
-    angular.module('app.components.tileGrid', []).component('tileGrid', {
-        templateUrl: 'src/components/tile-grid/tile-grid.html',
-        bindings: {
-            itemsList: '<?',
-            summarize: '<?'
-        },
-        controller: function TileGridCtrl() {
-            const getItemWidth = () => {
-                const pixels = window.innerWidth;
-                let columnCount = 1;
-
-                if (pixels > 1750) {
-                    columnCount = 4;
-                } else if (pixels < 1750 && pixels > 1200) {
-                    columnCount = 3;
-                } else if (pixels < 1200 && pixels > 900) {
-                    columnCount = 2;
+                for (iter = 0; iter < tilesListLength; iter++) {
+                    tilesList[iter].style.width = itemWidth;
                 }
+            }
+        };
 
-                return (window.innerWidth - (columnCount + 2) * 20) / columnCount;
-            };
+        _getItemWidth() {
+            const pixels = window.innerWidth;
+            let columnCount = 1;
 
-            this.$onChanges = (changes) => {
-                if (changes && changes.itemsList) {
-                    const itemWidth = getItemWidth() + 'px',
-                        tilesList = document.getElementsByClassName('item-container'),
-                        tilesListLength = tilesList.length;
-                    let iter;
+            if (pixels > 1750) {
+                columnCount = 4;
+            } else if (pixels < 1750 && pixels > 1200) {
+                columnCount = 3;
+            } else if (pixels < 1200 && pixels > 900) {
+                columnCount = 2;
+            }
 
-                    for (iter = 0; iter < tilesListLength; iter++) {
-                        tilesList[iter].style.width = itemWidth;
-                    }
-                }
-            };
-        },
-        controllerAs: 'tileGrid'
-    });
-})();
+            return (window.innerWidth - (columnCount + 2) * 20) / columnCount;
+        };
+    },
+    controllerAs: 'tileGrid'
+});
